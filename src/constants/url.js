34afd721b721b60606ecd2e4bccc0df22eca0752
mlnj27 <yjm9575m89@gmail.com>
2023-10-19 01:31:16 +0900

    feat: 구글 플레이스토어 오류해결 수정본

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,7 +1,7 @@
 const backendURL = process.env.backendURL || "http://localhost:3000"
-const backendURL = "http://10.0.2.2:3000"
+const testURL = "http://10.0.2.2:3000"
 const defaultImg = "https://postfiles.pstatic.net/MjAyMzEwMDFfNjIg/MDAxNjk2MTYzMzcxMjc2.oVup_aS64ZNVcQnNnkmev1v1hFJSFyXTAv243hyRa1kg.QM5VNyfuGV_gZCTzZUp8SrP2XzMbjnruGZjW9zkU3Eog.PNG.pineapple7358/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7_2023-10-01_%EC%98%A4%ED%9B%84_9.29.27.png?type=w966"
 
 module.exports.backendURL = backendURL;
-module.exports.backendURL = backendURL;
+module.exports.testURL = testURL;
 module.exports.defaultImg = defaultImg
